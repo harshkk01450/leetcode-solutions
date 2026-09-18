04dc6a6c0c7e73325400940a0c211c1b14d0ff88
Harsh Raj Kaushik <harshkk01450@gmail.com>
2026-09-18 23:20:16 +0530

    Time: 3 ms (52.44%), Space: 11.9 MB (49.27%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<int, int> mp;
+        unordered_map<char, int> mp;
         for(int i=0; i<magazine.size(); i++) {
-            if(mp.find(magazine[i]) == mp.end()) {
-                mp[magazine[i]] = 1;
-            } else {
-                mp[magazine[i]]++;
-            }
+            mp[magazine[i]]++;
         }
 
         for(int i=0; i<ransomNote.size(); i++) {
-            if(mp.find(ransomNote[i]) == mp.end() || mp[ransomNote[i]] == 0) return false;
+            if(mp[ransomNote[i]] == 0) return false;
             else {
                 mp[ransomNote[i]]--;
             }
